cecb778e95ffe1e275e738e57da224676f29b94b
Akash Bisariya <bigakash@gmail.com>
2021-08-30 19:31:53 +0530

    Added Json sample data

---
+++ src/component/blogComment.js
@@ -0,0 +1,34 @@
+export const blogCommentData = {
+  "comments": [
+    {
+      "id": 100,
+      "blogId": 1,
+      "comment": "This is comment 1",
+      "isActive": true
+    },
+    {
+      "id": 101,
+      "blogId": 2,
+      "comment": "This is comment 2",
+      "isActive": true
+    },
+    {
+      "id": 102,
+      "blogId": 3,
+      "comment": "This is comment 3",
+      "isActive": true
+    },
+    {
+      "id": 103,
+      "blogId": 1,
+      "comment": "This is comment 4",
+      "isActive": true
+    },
+    {
+      "id": 104,
+      "blogId": 1,
+      "comment": "This is comment 5",
+      "isActive": false
+    },
+  ]
+}
---
+++ src/component/blogData.js
@@ -0,0 +1,19 @@
+export const blogSampleData = {
+    "blog": [
+      {
+        "id": 1,
+        "title": "Nagarro Global",
+        "description": "Nagarro Gloabl is a MNC"
+      },
+      {
+        "id": 2,
+        "title": "Nagarro Noida",
+        "description": "Nagarro is a MNC in Noida"
+      },
+      {
+        "id": 3,
+        "title": "Nagarro Jaipur",
+        "description": "Nagarro is a MNC in Jaipur"
+      }
+    ]
+} 
